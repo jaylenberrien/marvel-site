54603893934f72e087500190d3695f5237906414
jaylenberrien <jaylenberrien.jb@gmail.com>
2024-01-07 11:22:39 -0500

    search bar makes variable

--- src/Pages/CharacterSearch.jsx
+++ src/Pages/CharacterSearch.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CharacterSearch = () => {
+
+  const publicKey = import.meta.env.VITE_UKEY;
+  
+  const [value, setValue] = useState("");
+  const click = () =>{
+    let userInput = value
+    console.log(userInput)
+  }
+
+  const change = evt =>{
+    setValue(evt.target.value)
+  }
+
   return (
-    <div class="page-container">CharacterSearch</div>
+    <div class="page-container">
+      <div>
+        CharacterSearch
+      </div>
+      <input type='text' id="submitCharInput" onChange={change} value={value}placeholder='I want to see ...'/>
+      <button id="submitCharSearch" onClick={click}>Sumbit</button>
+    </div>
   )
 }
 
